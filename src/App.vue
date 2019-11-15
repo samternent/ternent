ba9c986481a8561b2b2218630ec9b3a958bbb02b
Sam Ternent <sam.ternent@gmail.com>
2019-11-15 14:58:45 +0000

    adds tail wind

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,77 @@
 <template>
   <div id="app">
-    <h1>ternent.dev</h1>
-    <p>domained</p>
+    <header id="header" class="fixed w-full z-10 top-0 bg-white border-b border-gray-400">
+      <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-4">
+        <div class="pl-4 flex items-center">
+            <a class="text-gray-900 text-base no-underline hover:no-underline font-extrabold text-xl"  href="#">
+              ST
+            </a>
+        </div>
+        <div class="pl-4 flex items-center">
+            <a class="text-gray-900 text-base no-underline hover:no-underline"  href="#">
+              ST
+            </a>
+        </div>
+      </div>
+    </header>
+    <nav class="w-full container flex flex-wrap border-b border-gray-400">
+
+    </nav>
+    <!--Container-->
+    <div class="w-full container flex flex-wrap mx-auto px-2 pl-0 pt-20 mt-2">
+      <nav>
+        <ul class="flex">
+          <li class="mr-6">
+            <a class="text-blue-500 hover:text-blue-800" href="#">Home</a>
+          </li>
+          <li class="mr-6">
+            <a class="text-blue-500 hover:text-blue-800" href="#">Timeline</a>
+          </li>
+          <li class="mr-6">
+            <a class="text-blue-500 hover:text-blue-800" href="#">Photos</a>
+          </li>
+          <li class="mr-6">
+            <a class="text-blue-500 hover:text-blue-800" href="#">Friends</a>
+          </li>
+          <li class="mr-6">
+            <a class="text-blue-500 hover:text-blue-800" href="#">Business</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div class="w-full container flex flex-wrap mx-auto pt-6">
+      <div class="w-full bg-gray-200 p-4 text-gray-800">
+        <vue-markdown>i am a ~~tast~~ **test**.</vue-markdown>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown';
+
 export default {
   name: 'app',
+  components: { VueMarkdown },
+  data: () => ({
+    selected: 1
+  }),
+  methods: {
+    select(i) {
+      this.selected = i;
+    }
+  }
 }
 </script>
 
-<style>
-@font-face {
-  font-family: 'Noah';
-  src:
-      url('./assets/fonts/Noah/Noah-Regular.eot'),
-      url('./assets/fonts/Noah/Noah-Regular.woff2') format('woff2'),
-      url('./assets/fonts/Noah/Noah-Regular.woff') format('woff'),
-      url('./assets/fonts/Noah/Noah-Regular.ttf') format('truetype');
-  font-weight: normal;
-  font-style: normal;
-}
-@font-face {
-  font-family: 'Noah Bold';
-  src:
-      url('./assets/fonts/Noah/Noah-Bold.eot'),
-      url('./assets/fonts/Noah/Noah-Bold.woff2') format('woff2'),
-      url('./assets/fonts/Noah/Noah-Bold.woff') format('woff'),
-      url('./assets/fonts/Noah/Noah-Bold.ttf') format('truetype');
-  font-weight: normal;
-  font-style: normal;
+<style lang="scss">
+
+@import url('https://fonts.googleapis.com/css?family=Alata&display=swap');
+
+body {
+  font-family: 'Alata', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
-h1 {
-  font-family: 'Noah Bold';
-  font-weight: bold;
-}
 </style>
